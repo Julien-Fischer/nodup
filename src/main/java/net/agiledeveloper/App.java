919aaa9c95a6cf7b2288d97882a7bd8c312515f4
Julien-Fischer <julien.fischer@agiledeveloper.net>
2025-07-12 23:30:40 +0200

    ♻️ Extract log formatter class

--- src/main/java/net/agiledeveloper/App.java
+++ src/main/java/net/agiledeveloper/App.java
@@ -15,10 +15,18 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Collection;
 import java.util.List;
+import java.util.logging.Formatter;
 import java.util.logging.Level;
+import java.util.logging.LogRecord;
 import java.util.logging.Logger;
+
+import static java.time.format.DateTimeFormatter.ofPattern;
 
 public class App {
 
@@ -42,7 +50,7 @@ public class App {
         String directory = args[0];
         Image[] images = at(directory);
         Collection<Collision> collisions = imageProcessor.detectCollisions(images);
-        logger.info(() -> "#".repeat(40));
+        logger.info(() -> "#".repeat(80));
         logger.info(() -> "Found %s collisions:".formatted(collisions.size()));
         for (Collision collision : collisions) {
             logger.info(() -> "" + collision);
@@ -82,8 +90,29 @@ public class App {
     private static void setLogLevel(Level level) {
         logger.setLevel(level);
         for (var handler : Logger.getLogger("").getHandlers()) {
+            handler.setFormatter(new MessageFormatter());
             handler.setLevel(level);
         }
+    }
+
+    public static class MessageFormatter extends Formatter {
+
+            private static final DateTimeFormatter DATE_TIME_FORMATTER = ofPattern("yyyy-MM-dd HH:mm:ss");
+
+            @Override
+            public String format(LogRecord logRecord) {
+                var date = LocalDateTime.ofInstant(
+                        Instant.ofEpochMilli(logRecord.getMillis()),
+                        ZoneId.systemDefault()
+                );
+                return String.format("[%s] %-7s %s - %s%n",
+                        date.format(DATE_TIME_FORMATTER),
+                        logRecord.getLevel().getName(),
+                        logRecord.getLoggerName(),
+                        formatMessage(logRecord)
+                );
+            }
+
     }
 
     private enum Collider {
